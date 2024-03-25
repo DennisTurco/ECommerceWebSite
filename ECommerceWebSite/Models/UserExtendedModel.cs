c2550847c42713900994d96bc25ca67dbdce69db
DennisTurco <dennisturco@gmail.com>
2024-03-25 10:00:37 +0100

    first commit

---
+++ ECommerceWebSite/Models/UserExtendedModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace ECommerceWebSite.Models
+{
+    public class UserExtendedModel : UserModel
+    {
+        public UserExtendedModel()
+        {
+            Claims = new List<string>();
+            Roles = new List<string>();
+        }
+        public List<string> Claims { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
---
+++ ECommerceWebSite/Models/UserModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceWebSite.Models
+{
+    // Do not use "[Required]" or "[Key]" because this does not rappresent directly the user model.
+
+    public class UserModel
+    {
+        public string? Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Name cannot exceded 50 characters")]
+        [MinLength(1, ErrorMessage = "FirstName is missing")]
+        [DisplayName("Firstname")]
+        public string? FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Surname cannot exceded 50 characters")]
+        [MinLength(1, ErrorMessage = "LastName is missing")]
+        [DisplayName("Lastname")]
+        public string? LastName { get; set; }
+
+        [EmailAddress]
+        [MinLength(1, ErrorMessage = "Email is missing")]
+        [DisplayName("Email")]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [MinLength(1, ErrorMessage = "Pasword is missing")]
+        [DisplayName("Password")]
+        public string? Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password doesn't match")]
+        [MinLength(1, ErrorMessage = "Pasword is missing")]
+        [DisplayName("Confirm Password")]
+        public string? ConfirmPassword { get; set; }
+
+        [DisplayName("Admin")]
+        public bool IsAdmin { get; set; }
+
+        [NotMapped]
+        [DisplayName("Full Name")]
+        public string? FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+    }
+}
