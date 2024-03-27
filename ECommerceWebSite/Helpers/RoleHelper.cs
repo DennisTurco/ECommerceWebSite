fb65410d2be9ba940094e6fe5f1600b38b69c74a
Dennis Turco <57963761+DennisTurco@users.noreply.github.com>
2024-03-27 09:19:05 +0100

    adding role to the user by code

---
+++ ECommerceWebSite/Helpers/RoleHelper.cs
@@ -0,0 +1,43 @@
+﻿using ECommerceWebSite.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+public class RoleHelper
+{
+	private readonly UserManager<ApplicationUser> _userManager;
+	private readonly RoleManager<IdentityRole> _roleManager;
+
+	public RoleHelper(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+	{
+		_userManager = userManager;
+		_roleManager = roleManager;
+	}
+
+	/*
+	 * This function must be called from a Task<IActionResult> function.
+	 * I suggets to include everything inside a try catch block. 
+	 * example of a possible call:
+	 * try
+     * {
+	 *		RoleHelper helper = new RoleHelper(_userManager, _roleManager);
+	 *		await helper.AssignUserToRole("a7eddd16-d58c-4527-921e-7af1f500a1a7", "Admin");
+	 * } catch (Exception ) { }
+	*/ 
+	public async Task AssignUserToRole(string userId, string roleName)
+	{
+		// check if the role exists, otherwise return
+		if (_roleManager.FindByNameAsync(roleName) == null)
+		{
+			return;
+		}
+
+		// obtain the user
+		var user = await _userManager.FindByIdAsync(userId);
+
+		// assign user to the rule
+		if (user != null && !await _userManager.IsInRoleAsync(user, roleName))
+		{
+			await _userManager.AddToRoleAsync(user, roleName);
+		}
+	}
+}
--- ECommerceWebSite/Program.cs
+++ ECommerceWebSite/Program.cs
@@ -1,84 +1,121 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using ECommerceWebSite.Areas.Identity.Data;
 using ECommerceWebSite.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDBContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDBContextConnection' not found.");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-// External login systems (https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/?view=aspnetcore-8.0&tabs=visual-studio)
-/*
-builder.Services.AddAuthentication()
-   .AddGoogle(options =>
-   {
-       IConfigurationSection googleAuthNSection =
-       config.GetSection("Authentication:Google");
-       options.ClientId = googleAuthNSection["ClientId"];
-       options.ClientSecret = googleAuthNSection["ClientSecret"];
-   })
-   .AddFacebook(options =>
-   {
-       IConfigurationSection FBAuthNSection =
-       config.GetSection("Authentication:FB");
-       options.ClientId = FBAuthNSection["ClientId"];
-       options.ClientSecret = FBAuthNSection["ClientSecret"];
-   })
-   .AddMicrosoftAccount(microsoftOptions =>
-   {
-       microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
-       microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
-   })
-   .AddTwitter(twitterOptions =>
-   {
-       twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
-       twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
-       twitterOptions.RetrieveUserDetails = true;
-   });
-*/
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace ECommerceWebSite
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var connectionString = builder.Configuration.GetConnectionString("ApplicationDBContextConnectionLaptop") ?? throw new InvalidOperationException("Connection string 'ApplicationDBContextConnection' not found.");
+
+			builder.Services.AddDistributedMemoryCache();
+			builder.Services.AddSession(options =>
+			{
+				options.IdleTimeout = TimeSpan.FromSeconds(10);
+				options.Cookie.HttpOnly = true;
+				options.Cookie.IsEssential = true;
+			});
+
+			builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+			builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false) // false = no confirmation required
+				.AddRoles<IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+			// External login systems (https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/?view=aspnetcore-8.0&tabs=visual-studio)
+			/*
+			builder.Services.AddAuthentication()
+			   .AddGoogle(options =>
+			   {
+				   IConfigurationSection googleAuthNSection =
+				   config.GetSection("Authentication:Google");
+				   options.ClientId = googleAuthNSection["ClientId"];
+				   options.ClientSecret = googleAuthNSection["ClientSecret"];
+			   })
+			   .AddFacebook(options =>
+			   {
+				   IConfigurationSection FBAuthNSection =
+				   config.GetSection("Authentication:FB");
+				   options.ClientId = FBAuthNSection["ClientId"];
+				   options.ClientSecret = FBAuthNSection["ClientSecret"];
+			   })
+			   .AddMicrosoftAccount(microsoftOptions =>
+			   {
+				   microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
+				   microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
+			   })
+			   .AddTwitter(twitterOptions =>
+			   {
+				   twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
+				   twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
+				   twitterOptions.RetrieveUserDetails = true;
+			   });
+			*/
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseRouting();
+			app.UseAuthentication();
+			app.UseAuthorization();
+			app.UseSession();
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapRazorPages();
+
+			// create as default 2 roles: Admin and User
+			using (var scope = app.Services.CreateScope())
+			{
+				var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+				var roles = new[] { "Admin", "SimpleUser" };
+
+				foreach (var role in roles)
+				{
+					// mi permette di creare i Ruoli all'interno del database se non gia' presenti
+					if (!await roleManager.RoleExistsAsync(role)) await roleManager.CreateAsync(new IdentityRole(role));
+				}
+			}
+
+			// create ad default a admin account
+			/*using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                string email = "admin@admin.com";
+                string password = "asdasd123A?";
+
+                if (await userManager.FindByEmailAsync(email) == null)
+                {
+                    var user = new IdentityUser();
+                    user.UserName = email;
+                    user.Email = email;
+
+                    await userManager.CreateAsync(user, password);
+
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }*/
+
+			app.Run();
+		}
+	}
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-
-// create as default 2 roles: Admin and SimpleUser
-//using (var scope = app.Services.CreateScope())
-//{
-//    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-//    var roles = new[] { "Admin", "SimpleUser" };
-
-//    foreach (var role in roles)
-//    {
-//        // mi permette di creare i Ruoli all'interno del database se non gia' presenti
-//        if (!await roleManager.RoleExistsAsync(role)) await roleManager.CreateAsync(new IdentityRole(role));
-//    }
-//}
-
-app.Run();
